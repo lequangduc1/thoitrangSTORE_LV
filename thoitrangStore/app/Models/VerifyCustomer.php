f755c1918db4631b66099a2d3cca1a5b9bb1a280
hoaitt <hoaitran21111999@gmail.com>
2022-12-09 14:33:55 +0700

    verify email

---
+++ thoitrangStore/app/Models/VerifyCustomer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class VerifyCustomer extends Model
+{
+    use HasFactory;
+
+    protected $table = 'verify_customers';
+
+    protected  $fillable = [
+        'customer_id',
+        'token'
+    ];
+
+    public function customer(){
+        return $this->belongsTo(khachhang::class, 'customer_id','id');
+    }
+}
